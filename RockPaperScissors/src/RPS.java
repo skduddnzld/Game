f772140ac777f8719872b7ee42f053ca317960ea
UESR <UESR@10.142.45.105>
2019-12-06 11:14:49 +0900

    Commit

---
+++ RockPaperScissors/src/RPS.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class RPS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-룰 설명-");
+		System.out.println("p는 포인터");
+		System.out.println("p가 -3일때 블루(b) 승!");
+		System.out.println("p가 3일때 레드(r) 승!");
+		System.out.print("가위바위보 게임 시작");
+		int p;
+		p = 0;
+		for (;;) {
+			System.out.println("포인트는 " + p);
+			int r;
+			r = 0;
+			int b;
+			b = 0;
+
+			System.out.println("1.가위 2.바위 3.보");
+			System.out.print("r의 선택 :");
+			r = sc.nextInt();
+			System.out.print("b의 선택 :");
+			b = sc.nextInt();
+// ========================r win=====================
+			if (r == 1 && b == 3) {
+				System.out.println("r 승!");
+				p += 1;
+			} else if (r == 2 && b == 1) {
+				System.out.println("r 승!");
+				p += 1;
+			} else if (r == 3 && b == 2) {
+				System.out.println("r 승!");
+				p += 1;
+			}
+// ========================b win=====================
+			else if (r == 3 && b == 1) {
+				System.out.println("b 승!");
+				p -= 1;
+			} else if (r == 1 && b == 2) {
+				System.out.println("b 승!");
+				p -= 1;
+			} else if (r == 2 && b == 3) {
+				System.out.println("b 승!");
+				p -= 1;
+			}
+// ===================================================			
+			else {
+				System.out.println("비김");
+			}
+//=========================END========================
+			if (p == 3) {
+				System.out.println("레드 승!");
+				return;
+			} else if (p == -3) {
+				System.out.println("블루 승!");
+				return;
+			}
+		}
+	}
+}
---
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class RockPaperScissors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random ran = new Random();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-·ê ¼³¸í-");
+		System.out.println("p´Â Æ÷ÀÎÅÍ");
+		System.out.println("p°¡ -3ÀÏ¶§ ºí·ç(b) ½Â!");
+		System.out.println("p°¡ 3ÀÏ¶§ ·¹µå(r) ½Â!");
+		System.out.print("°¡À§¹ÙÀ§º¸ °ÔÀÓ ½ÃÀÛ");
+		int p;
+		p = 0;
+		for (;;) {
+			System.out.println("Æ÷ÀÎÆ®´Â " + p);
+			int r;
+			r = 0;
+			int b = (int) (Math.random() * 2 + 1);
+			System.out.println("1.°¡À§ 2.¹ÙÀ§ 3.º¸");
+			System.out.print("rÀÇ ¼±ÅÃ :");
+			r = sc.nextInt();
+// ========================r win=====================
+			if (r == 1 && b == 3) {
+				System.out.println("r ½Â!");
+				p += 1;
+			} else if (r == 2 && b == 1) {
+				System.out.println("r ½Â!");
+				p += 1;
+			} else if (r == 3 && b == 2) {
+				System.out.println("r ½Â!");
+				p += 1;
+			}
+// ========================b win=====================
+			else if (r == 3 && b == 1) {
+				System.out.println("b ½Â!");
+				p -= 1;
+			} else if (r == 1 && b == 2) {
+				System.out.println("b ½Â!");
+				p -= 1;
+			} else if (r == 2 && b == 3) {
+				System.out.println("b ½Â!");
+				p -= 1;
+			}	
+// ===================================================			
+			else {
+				System.out.println("ºñ±è");
+			}
+//=========================END========================
+			if (p == 3) {
+				System.out.println("·¹µå ½Â!" + p);
+				return;
+			} else if (p == -3) {
+				System.out.println("ºí·ç ½Â!" + p);
+				return;
+			}
+		}
+	}
+}
